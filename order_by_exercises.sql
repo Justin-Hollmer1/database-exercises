392dfc68a1a5a0fb271ab37862e266d6ab6c4cb2
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-26 15:47:46 -0500

    completed the functions exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -9,4 +9,3 @@ ORDER BY emp_no DESC;
 SELECT * FROM employees WHERE (last_name LIKE ('E%') AND last_name LIKE ('%E'));
 SELECT * FROM employees WHERE last_name LIKE ('%q%');
 SELECT * FROM employees WHERE last_name LIKE ('%q%') AND last_name NOT LIKE ('%qu%');
-SELECT first_name FROM employees WHERE first_name REGEXP ''
