d75a7392d48994a8d158906340e8bc53e1235dea
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-26 12:02:58 -0500

    completed the where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+DESCRIBE employees;
+SELECT * FROM employees
+    WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'MAYA')
+    AND (gender = 'M');
+SELECT * FROM employees WHERE (last_name LIKE ('E%') OR last_name LIKE ('%E'));
+SELECT * FROM employees WHERE (last_name LIKE ('E%') AND last_name LIKE ('%E'));
+SELECT * FROM employees WHERE last_name LIKE ('%q%');
+SELECT * FROM employees WHERE last_name LIKE ('%q%') AND last_name NOT LIKE ('%qu%');
