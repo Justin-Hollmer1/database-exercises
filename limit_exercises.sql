44f7050a78b46395691b59e68fb01d7d026c5c90
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-26 15:23:07 -0500

    completed the limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC;
+DESCRIBE salaries;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
+
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 4 OFFSET 46;
+
